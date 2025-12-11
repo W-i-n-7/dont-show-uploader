3939bd7d6d40f091e44748ee3d72e14be5a20070
Win_7 <90262580+W-i-n-7@users.noreply.github.com>
2025-12-11 16:27:52 +0200

    Add plugin

---
+++ dist/plugin.js
@@ -0,0 +1,28 @@
+exports.version = 1.0
+exports.apiRequired = 10.3 // api.ctxBelongsTo
+exports.description = "Only show file uploader information to allowed users (or no one)"
+
+exports.config = {
+    allowedusers: {
+        type: 'username',
+        multiple: true,
+        label: 'Allowed users'
+    }
+}
+
+exports.init = async api => ({
+    async middleware(ctx) {
+        if (ctx.path === '/~/api/get_file_details') {
+			var array = api.getConfig('allowedusers');
+            if (array && api.ctxBelongsTo(ctx, array)) return;
+            return () => {
+                if (ctx.body && ctx.body.details) {
+                    for (const det of ctx.body.details) {
+                        delete det.upload;
+                    }
+                }
+            };
+        }
+    }
+});
+
